3896e5d753b51ad9f71a60f60842a4b5085f2854
Brad <brad@brad.tw>
2019-07-27 14:04:25 +0800

    03

---
+++ app/src/main/java/tw/org/iii/appps/brad06/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package tw.org.iii.appps.brad06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
